89e4ddccfae7df433e8fac9abc5dd0d662bb4828
Ollie24216 <24216or@hvhs.school.nz>
2026-06-21 22:44:36 +0000

    Jun 21, 2026, 10:44 PM

--- script.js
+++ script.js
@@ -6,38 +6,33 @@ let money = 2;
 
 let birthYear = year - age;
 let oldAge = age + 10;
-
 let halfMoney = money / 2;
 
 let OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 
-
+// 1. This runs automatically when the page loads
 function start() {
-  
+  displayWelcome(Username, pocketmoney);
 }
-
 
 function displayWelcome(_name, _money) {
   OUTPUT.innerHTML += "<p>Welcome to the page " + _name + "</p>";
   OUTPUT.innerHTML += "<p>You have $" + _money + " in pocket money</p>";
 }
 
-
 function getFormInput() {
-  
   let name = document.getElementById("nameField").value;
-  
-  
   let enteredPocketmoney = Number(document.getElementById("pocketmoneyField").value);
 
-  
-  OUTPUT.innerHTML += "<p>Hello customer" + "</p>";
+  OUTPUT.innerHTML += "<p>Hello customer " + name + "</p>"; // Fixed to actually use the name variable
   OUTPUT.innerHTML += "<p>You have $" + enteredPocketmoney + " money </p>";
 
-
   if (enteredPocketmoney >= 4) {
-    OUTPUT.innerHTML += "<p>you can afford to buy the fries!</p>";
+    OUTPUT.innerHTML += "<p>You can afford to buy the fries!</p>";
   } else {
-    OUTPUT.innerHTML += "<p>You can't afford the fries .</p>";
+    OUTPUT.innerHTML += "<p>You can't afford the fries.</p>";
   }
 }
+
+// 2. This tells the browser to trigger the start function on load
+window.onload = start;
