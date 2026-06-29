5e2374ff9735f61c1c79a7f1fcbb3f6bb2bde091
Ollie24216 <24216or@hvhs.school.nz>
2026-06-29 23:48:58 +0000

    Jun 29, 2026, 11:48 PM

--- script.js
+++ script.js
@@ -2,24 +2,20 @@ let OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 
 function getFormInput() {
 
-    // Get values from the form
     let customerName = document.getElementById("customerName").value;
     let order = document.getElementById("nameField").value;
     let money = Number(document.getElementById("pocketmoneyField").value);
 
+    OUTPUT.innerHTML = `
+        <h2>Order Summary</h2>
+        <p><b>Name:</b> ${customerName}</p>
+        <p><b>Order:</b> ${order}</p>
+        <p><b>Money:</b> ${money}</p>
+    `;
 
-    OUTPUT.innerHTML = "";
-
-
-    OUTPUT.innerHTML += "<h2>Order Summary</h2>";
-    OUTPUT.innerHTML += "<p><b>Name:</b> " + customerName + "</p>";
-    OUTPUT.innerHTML += "<p><b>Order:</b> " + order + "</p>";
-    OUTPUT.innerHTML += "<p><b>Money:</b> $" + money + "</p>";
-
- 
     if (money >= 4) {
         OUTPUT.innerHTML += "<p>You can afford the fries!</p>";
     } else {
         OUTPUT.innerHTML += "<p>You can't afford the fries.</p>";
     }
-  }
+}
