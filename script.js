3c32eab2d302a82d80bc34aca16499497b2c6fd0
Ollie24216 <24216or@hvhs.school.nz>
2026-06-29 23:44:10 +0000

    Jun 29, 2026, 11:44 PM

--- script.js
+++ script.js
@@ -1,33 +1,25 @@
-let Username = "Ollie";
-let pocketmoney = 3;
-
 let OUTPUT = document.getElementById("spaceForJavaScriptOutput");
-
-function start() {
-    displayWelcome(Username, pocketmoney);
-}
-
-function displayWelcome(_name, _money) {
-    OUTPUT.innerHTML = "<p>Welcome to Ollie's Cafe, " + _name + "!</p>";
-    OUTPUT.innerHTML += "<p>You have $" + _money + " in pocket money.</p>";
-}
 
 function getFormInput() {
 
+    // Get values from the form
     let customerName = document.getElementById("customerName").value;
     let order = document.getElementById("nameField").value;
-    let enteredPocketmoney = Number(document.getElementById("pocketmoneyField").value);
+    let money = Number(document.getElementById("pocketmoneyField").value);
+
 
     OUTPUT.innerHTML = "";
 
-    OUTPUT.innerHTML += "<h2>Order Summary</h2>";
-    OUTPUT.innerHTML += "<p><strong>Name:</strong> " + customerName + "</p>";
-    OUTPUT.innerHTML += "<p><strong>Order:</strong> " + order + "</p>";
-    OUTPUT.innerHTML += "<p><strong>Money:</strong> $" + enteredPocketmoney + "</p>";
 
-    if (enteredPocketmoney >= 4) {
-        OUTPUT.innerHTML += "<p>✅ You can afford the fries!</p>";
+    OUTPUT.innerHTML += "<h2>Order Summary</h2>";
+    OUTPUT.innerHTML += "<p><b>Name:</b> " + customerName + "</p>";
+    OUTPUT.innerHTML += "<p><b>Order:</b> " + order + "</p>";
+    OUTPUT.innerHTML += "<p><b>Money:</b> $" + money + "</p>";
+
+ 
+    if (money >= 4) {
+        OUTPUT.innerHTML += "<p>You can afford the fries!</p>";
     } else {
-        OUTPUT.innerHTML += "<p>❌ You can't afford the fries.</p>";
+        OUTPUT.innerHTML += "<p>You can't afford the fries.</p>";
     }
-}
+  }
